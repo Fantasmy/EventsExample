90f9406ea34ac1add3669c2a74283ab3f4b70d74
Jenny <fantazmit@gmail.com>
2018-07-23 14:00:11 +0200

    finished

---
+++ EventsExample/MultipleOfFiveEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsExample
+{
+   public class MultipleOfFiveEventArgs : EventArgs
+    {
+        public MultipleOfFiveEventArgs(int iTotal)
+        { 
+            Total = iTotal; // se podrá llamar des del constructor como des de la propiedad
+        }
+
+        public int Total { get; set;}
+
+    }
+}
